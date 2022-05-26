d20863c1c51e927c7e25b1c6a1ab3afff4b6896c
patrickpoli <patrickpolicarpio08@gmail.com>
2022-05-26 21:42:56 +0800

    Added custom command - for system initial setup commands

--- app/Models/Emergencies/EmergencyLog.php
+++ app/Models/Emergencies/EmergencyLog.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class EmergencyLog extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    /** Relationshps */
+    public function emergency_sos()
+    {
+        return $this->belongsTo('App\Models\Emergencies\EmergencySos');
+    }
+
+    public function emergency_type()
+    {
+        return $this->belongsTo('App\Models\Emergencies\EmergencyType');
+    }
 }
