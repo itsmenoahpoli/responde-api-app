699e1c02583a77fb6a76cbd2a90a390bb3143454
patrickpoli <patrickpolicarpio08@gmail.com>
2022-05-31 10:15:57 +0800

    Emergency sms templates seeder (to be improved)

--- database/seeders/EmergencySmsTemplatesSeeder.php
+++ database/seeders/EmergencySmsTemplatesSeeder.php
@@ -4,8 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-use App\Models\Emergencies\EmergencyTypes;
+use App\Models\Emergencies\EmergencyType;
 use App\Models\Emergencies\EmergencySmsTemplate;
+
 
 class EmergencySmsTemplatesSeeder extends Seeder
 {
@@ -16,21 +17,24 @@ class EmergencySmsTemplatesSeeder extends Seeder
      */
     public function run()
     {
-        $emergencyTypes = EmergencyTypes::all();
+        $emergencyTypes = EmergencyType::all();
 
         foreach ($emergencyTypes as $emergencyType)
         {
             EmergencySmsTemplate::create([
                 'emergency_type_id' => $emergencyType->id,
                 'code' => 'SMS-'.$emergencyType->code,
-                'name' => 'SMS-TEMPLATE-'.$emergentyType->name,
+                'name' => 'SMS-TEMPLATE-'.$emergencyType->name,
                 'message' => $this->makeSmsTemplate($emergencyType)
             ]);
         }
     }
 
-    public function makeSmsTemplate($emergentyType)
+    public function makeSmsTemplate($emergencyType)
     {
-        return '';
+        return <<<EOD
+            [$emergencyType->code] $emergencyType->name - REPORTED FROM LOCATION: Longtitude 121.023415 Latitude 14.556586 (Makati, Metro Manila, Philippines)
+            BY USER user::name AT report::currentTime
+        EOD;
     }
 }
